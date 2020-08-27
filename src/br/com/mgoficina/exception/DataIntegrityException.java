dfcb0515dc9d0b16bfd24bf0433210906bb2251b
ggwadera <16023489+ggwadera@users.noreply.github.com>
2020-08-27 16:18:41 -0300

    refatorar exceções

--- src/br/com/mgoficina/exception/DataIntegrityException.java
+++ src/br/com/mgoficina/exception/DataIntegrityException.java
@@ -2,28 +2,7 @@ package br.com.mgoficina.exception;
 
 public class DataIntegrityException extends RuntimeException {
 
-	public DataIntegrityException() {
-		// TODO Auto-generated constructor stub
+	public DataIntegrityException(String message) {
+		super(message);
 	}
-
-	public DataIntegrityException(String arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
-
-	public DataIntegrityException(Throwable arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
-
-	public DataIntegrityException(String arg0, Throwable arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
-
-	public DataIntegrityException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
-	}
-
 }
